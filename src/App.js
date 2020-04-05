364c28ee5d497abb523924668749b4c504d86b11
Jorge Calvo <jorgeecg10@gmail.com>
2020-04-05 12:14:32 -0500

    Refactor App.js & README.md

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Redux Practice</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="container">
+    <h1>Redux Practice</h1>
+  </div>
+);
 
 export default App;
